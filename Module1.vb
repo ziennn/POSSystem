1cc463300055e1bcdbfcac48745304ab6c0fc35d
Crizien Navarro <66468156+ziennn@users.noreply.github.com>
2022-06-04 12:01:54 +0800

    Update Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -12,8 +12,7 @@ Module Module1
         cn = New MySqlConnection
         With cn
             'enter all connection properties here
-            .ConnectionString = "server=db4free.net;user id=kiessnacks;password=kiessnacks;database=kiessnacksposdb"
-            '.ConnectionString = "server=sql207.epizy.com;user id=epiz_31844092;password=Nv5QxaNabZAK2;database=epiz_31844092_kiessnackspos"
+            
         End With
     End Sub
 End Module
